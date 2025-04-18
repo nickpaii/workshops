6e048f3ba915996242c940c425093504d14e99a7
Gai'Nicholas Patton <nickpatton1999@yahoo.com>
2025-04-18 15:46:02 -0400

    added exercises to github

---
+++ FinancialCalculators/src/main/java/com/pluralsight/MortgageCalculator.java
@@ -0,0 +1,24 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class MortgageCalculator {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        /*Calculator will accept principal, interest rate, and loan length from user
+
+        Calculator will display expected monthly payment and total interest
+
+        Compound interest formula: M=Px(i(1+i)^n / ((1+i)^n)-1)
+
+        APR: .07625, Number of Monthly Payments formula: 12*y, Monthly Interest Rate formula: r/12
+        Total interest: (M*n)-P
+         */
+
+
+    }
+
+
+}
